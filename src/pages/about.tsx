c7a6c08631e82bcf864dd947f15eb0d5c41e018b
jmwalsh91 <jordanmwalsh@gmail.com>
2022-09-28 22:55:52 -0600

    task: aboutcard base

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import AboutCard from '../components/AboutCard'
 
 
 
 function About() {
   return (
-    <div className='container mx-auto bg-cyan-500'>
+    <div className='min-h-screen min-w-full flex flex-col bg-cyan-500'>
+        <AboutCard />
         <p className='text-2xl text-gray-200'>Second page.</p>
         <button className='bg-slate-200 hover:bg-green-300 text-slate-800 font-bold py-2 px-4 rounded'>
           Button
