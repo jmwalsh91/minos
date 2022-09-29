800edadc3b966c8ad091e330e31807f7d3184444
jmwalsh91 <jordanmwalsh@gmail.com>
2022-09-28 23:09:22 -0600

    task: Link to home

--- src/components/AboutCard.tsx
+++ src/components/AboutCard.tsx
@@ -1,24 +1,25 @@
-import React from 'react'
+import React from "react";
 
 type Props = {
-    title: string;
-    description: string;
-}
+  title: string;
+  description: string;
+};
 
-function AboutCard({title, description }: Props) {
+function AboutCard({ title, description }: Props) {
   return (
-    <div className='container bg-slate-100  h-96 w-96 shadow-2xl border-x-4 border-x-black border-y-4 border-y-slate-800 m-10'>
-        <div className='flex flex-col items-center justify-between p-4'>
-            <h1 className='text-5xl  leading-normal font-extrabold text-black'>
-       {title}
+    <div className="container bg-slate-100  h-96 w-96 shadow-2xl border-x-4 border-x-black border-y-4 border-y-slate-800 m-10">
+      <div className="flex flex-col items-center justify-between p-4">
+        <h1 className="text-5xl  leading-normal font-extrabold text-black">
+          {title}
         </h1>
-        <p className='text-lg text-gray-700 text-ellipsis '>
-{description}
-        </p>
-        <button className="p-4 m-4 self-center mix-blend-multiply bg-gray-300 opacity-80 hover:opacity-90 hover:border-cyan-600 border-4 border-black mt-10"> Learn More </button>
-        </div>
-        </div>
-  )
+        <p className="text-lg text-gray-700 text-ellipsis ">{description}</p>
+        <button className="p-4 m-4 self-center mix-blend-multiply bg-gray-300 opacity-80 hover:opacity-90 hover:border-cyan-600 border-4 border-black ">
+          {" "}
+          Learn More{" "}
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default AboutCard
+export default AboutCard;
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 import AboutCard from '../components/AboutCard'
 
@@ -6,11 +7,17 @@ import AboutCard from '../components/AboutCard'
 function About() {
   return (
     <div className='min-h-screen min-w-full flex flex-col bg-cyan-500'>
-        <AboutCard />
+        <div className='flex flex-row justify-center items-center'>
+        <AboutCard title="An Example" description='Science and mathematics Run parallel to reality, they symbolize it, they squint at it, They never touch it: consider what an explosion Would rock the bones of men into little white fragments and unsky the world If any mind for a moment touch truth.' />
+        <AboutCard title="Another" description='Nature knows that people are a tide that swells and in time will ebb, and all their works dissolve ... As for us: We must uncenter our minds from ourselves. We must unhumanize our views a little and become confident as the rock and ocean that we are made from.' />
+        <AboutCard title="Same" description='Science and mathematics Run parallel to reality, they symbolize it, they squint at it, They never touch it: consider what an explosion Would rock the bones of men into little white fragments and unsky the world If any mind for a moment touch truth.' />
+        </div>
         <p className='text-2xl text-gray-200'>Second page.</p>
-        <button className='bg-slate-200 hover:bg-green-300 text-slate-800 font-bold py-2 px-4 rounded'>
+        <Link href="/">
+            <button className='bg-slate-200 hover:bg-green-300 text-slate-800 font-bold py-2 px-4 rounded'>
           Button
         </button>
+            </Link>
     </div>
   )
 }
