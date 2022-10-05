ba66e8c9e1be9d27b0ac025f01bd9300c395c3d8
jmwalsh91 <jordanmwalsh@gmail.com>
2022-10-05 00:00:00 -0600

    task: form ui in messenger

--- src/components/Messager.tsx
+++ src/components/Messager.tsx
@@ -4,7 +4,12 @@ import React from 'react'
 
 function Messager() {
   return (
-    <div>Messager</div>
+    <div className='bg-blue-600 shadow-md w-96'>
+
+        <h2 className='text-2xl text-gray-200'>Messager</h2>
+        <textarea className="textarea textarea-primary" placeholder="from"></textarea>
+        <textarea className="textarea textarea-primary" placeholder="message"></textarea>
+    </div>
   )
 }
 
