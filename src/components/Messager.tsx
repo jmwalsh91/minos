33ac1ef7d3b934982214881c35a479235b16c298
jmwalsh91 <jordanmwalsh@gmail.com>
2022-10-04 23:57:23 -0600

    task: messager component init

---
+++ src/components/Messager.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+
+function Messager() {
+  return (
+    <div>Messager</div>
+  )
+}
+
+export default Messager
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,40 +21,9 @@ const Home: NextPage = () => {
           Create <span className="text-purple-300">T3</span> App
         </h1>
         <Link href="/about"><button className="bg-purple-600 border-x-blue p-3">about</button></Link>
-        <Link href="/about"><button className="bg-blue-600 border-x-blue p-3">Resources</button></Link>
+        <Link href="/resources"><button className="bg-blue-600 border-x-blue p-3">Resources</button></Link>
         <p className="text-2xl text-gray-700">This stack uses:</p>
-        <div className="grid gap-3 pt-3 mt-3 text-center md:grid-cols-2 lg:w-2/3">
-          <TechnologyCard
-            name="NextJS"
-            description="The React framework for production"
-            documentation="https://nextjs.org/"
-          />
-          <TechnologyCard
-            name="TypeScript"
-            description="Strongly typed programming language that builds on JavaScript, giving you better tooling at any scale"
-            documentation="https://www.typescriptlang.org/"
-          />
-          <TechnologyCard
-            name="TailwindCSS"
-            description="Rapidly build modern websites without ever leaving your HTML"
-            documentation="https://tailwindcss.com/"
-          />
-          <TechnologyCard
-            name="tRPC"
-            description="End-to-end typesafe APIs made easy"
-            documentation="https://trpc.io/"
-          />
-          <TechnologyCard
-            name="Next-Auth"
-            description="Authentication for Next.js"
-            documentation="https://next-auth.js.org/"
-          />
-          <TechnologyCard
-            name="Prisma"
-            description="Build data-driven JavaScript & TypeScript apps in less time"
-            documentation="https://www.prisma.io/docs/"
-          />
-        </div>
+        
         <div className="pt-6 text-2xl text-blue-500 flex justify-center items-center w-full">
           {hello.data ? <p>{hello.data.greeting}</p> : <p>Loading..</p>}
         </div>
