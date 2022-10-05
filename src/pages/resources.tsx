424461e7fb647b7d5946977ca4a017e8213a379a
jmwalsh91 <jordanmwalsh@gmail.com>
2022-10-05 02:29:05 -0600

    task: loading conditional, refactor

--- src/pages/resources.tsx
+++ src/pages/resources.tsx
@@ -4,12 +4,12 @@ import { trpc } from '../utils/trpc';
 
 
 
-function resources() {
+function Resources() {
     const sayWhat =  trpc.useQuery(["example.sayThing"]);
   return (
     <div>
         <h1 className="text-4xl font-bold text-center">Resources</h1>
-        <p>{sayWhat.data}</p>
+        <p>{sayWhat.isFetching? "loading..." : sayWhat.data}</p>
         <div className="grid gap-3 pt-3 mt-3 text-center md:grid-cols-2 lg:w-2/3">
             <AboutCard title="Writings" description="Texts about educations" />
             <AboutCard title="Talks" description="Talky talk talk talk--on camera!" />
@@ -19,4 +19,4 @@ function resources() {
   )
 }
 
-export default resources
+export default Resources
